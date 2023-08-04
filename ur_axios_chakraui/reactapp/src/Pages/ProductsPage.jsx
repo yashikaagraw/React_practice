5489db787f06c439180c9705b977acee5ad7dd46
yashika_agrawal_pw11_021 <yashikaagrawal23@gamil.com>
2023-08-04 17:21:24 +0530

    react

--- ur_axios_chakraui/reactapp/src/Pages/ProductsPage.jsx
+++ ur_axios_chakraui/reactapp/src/Pages/ProductsPage.jsx
@@ -1,5 +1,7 @@
 import React, {useEffect, useReducer} from 'react'
-
+import axios from 'axios';
+import { Box, Heading } from '@chakra-ui/react'
+//in useState, dispatch change the function state
 const initialState = {
   products: [],
   loading: false,
@@ -7,23 +9,72 @@ const initialState = {
 }
 
 const reducer= (state, action) => {
+  //product which have empty array
+  //console.log(state);
+  //console.log(action);
+  switch(action.type){
+ case "FETCH_REQUEST" : return {
+...state,
+loading: true,
+error: null,
+    }
+    case "FETCH_SUCCESS" : return{
+      ...state,
+      products: action.payload,
+      loading: false,
+      error: null,
+    }
+    case "FETCH_FAILURE" : return{
+      ...state,
+
+    }
+default:
+  throw Error("Unknown action:" + action.type)
+
+  }
+}
   //dispatch
 //making request to get products data
 
 
 const ProductsPage = () => {
   const [state, dispatch] = useReducer(reducer, initialState);
+  console.log(state);
+  const {products, loading, error } = state;
   //api(fakestore),
   //set state- state management 
   //useState, useReducer
   //render data on to the ui
+  const getData=()=> {
+    //loading is true 
+    //not use payload bcoz we are no sending any data here
+    //network call
+    dispatch({type: "FETCH_REQUEST"});
+    axios
+    .get(`https://fakestoreapi.com/products`)
+    .then((response)=> {
+      //console.log(response);
+      dispatch({type: "FETCH_SUCCESS", payload: response.data});
+    })
+    .catch((error) => {
+      dispatch({type: "FETCH_FAILURE", payload:error.message });
+    });
+  };
+  useEffect(()=> {
+  getData();
+  }, []);
+  console.log(products);
+  
 
   return (
-    <div>
-      <h1>Products Page</h1>
-    </div>
+    <Box>
+      <Heading>Products Page</Heading>
+    </Box>
   )
 }
 
-export default ProductsPage
+export default ProductsPage;
+//api call --> fakestore api
+//set state--> statemanagement --> useState, usereducer
+//render data on to the ui
 
